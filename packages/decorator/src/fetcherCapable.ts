757a2461ea519cb9b8b16e17e310b1f42c8826c4
Ahoo Wang <ahoowang@qq.com>
2025-08-27 20:15:34 +0800

    feat(decorator): add FetcherCapable interface and support fetcher override
    - Add FetcherCapable interface to define objects that can have a fetcher
    - Implement fetcher override capability in API metadata and request executor
    - Add getFetcher function to resolve fetcher instances
    - Update API decorator and request executor to use the new FetcherCapable interface

---
+++ packages/decorator/src/fetcherCapable.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Fetcher, fetcherRegistrar } from '@ahoo-wang/fetcher';
+
+/**
+ * Interface that defines a capability for objects that can have a fetcher.
+ * This interface is typically used to mark components or objects that can perform fetching operations
+ * and may need access to fetcher functionality.
+ */
+export interface FetcherCapable {
+  /**
+   * Optional fetcher property that can be either a string identifier or a Fetcher instance.
+   * When present, this property indicates the fetcher associated with the implementing object.
+   */
+  fetcher?: string | Fetcher;
+}
+
+/**
+ * Gets a Fetcher instance based on the provided fetcher parameter.
+ *
+ * @param fetcher - A string identifier or Fetcher instance to resolve
+ * @returns A Fetcher instance if found, undefined otherwise
+ */
+export function getFetcher(fetcher?: string | Fetcher): Fetcher | undefined {
+  // Return undefined if no fetcher is provided
+  if (!fetcher) {
+    return undefined;
+  }
+
+  // Return the fetcher directly if it's already a Fetcher instance,
+  // otherwise resolve it through the fetcher registrar
+  return fetcher instanceof Fetcher ? fetcher : fetcherRegistrar.requiredGet(fetcher);
+}
--- packages/decorator/src/index.ts
+++ packages/decorator/src/index.ts
@@ -1,6 +1,8 @@
 export * from './apiDecorator';
 export * from './endpointDecorator';
+export * from './fetcherCapable';
 export * from './parameterDecorator';
 export * from './reflection';
 export * from './requestExecutor';
 export * from './generated';
+
