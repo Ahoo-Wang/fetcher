50154da646e78dca220053e8007648b3321fcb69
Ahoo Wang <ahoowang@qq.com>
2025-08-29 07:32:42 +0800

    feat(cosec): add JWT parsing functionality
    - Implement JwtPayload interface for standard JWT claims
    - Add parseJwtPayload function to decode and parse JWT tokens
    - Include unit tests for JWT parsing functionality

--- packages/cosec/src/index.ts
+++ packages/cosec/src/index.ts
@@ -15,6 +15,7 @@ export * from './cosecRequestInterceptor';
 export * from './cosecResponseInterceptor';
 export * from './deviceIdStorage';
 export * from './idGenerator';
+export * from './jwts';
 export * from './inMemoryStorage';
 export * from './tokenRefresher';
 export * from './tokenStorage';
