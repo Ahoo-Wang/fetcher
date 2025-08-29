4d4febba91d758892f0a5475a873cfa5de8114db
Ahoo Wang <ahoowang@qq.com>
2025-08-29 12:08:02 +0800

    build(decorator): add reflect-metadata as external dependency
    - Add 'reflect-metadata' to external dependencies in vite.config.ts
    - This change prevents bundling reflect-metadata with the decorator package

--- packages/decorator/vite.config.ts
+++ packages/decorator/vite.config.ts
@@ -10,11 +10,12 @@ export default defineConfig({
       fileName: format => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['@ahoo-wang/fetcher', '@ahoo-wang/fetcher-eventstream'],
+      external: ['@ahoo-wang/fetcher', '@ahoo-wang/fetcher-eventstream', 'reflect-metadata'],
       output: {
         globals: {
           '@ahoo-wang/fetcher': 'Fetcher',
           '@ahoo-wang/fetcher-eventstream': 'FetcherEventStream',
+          'reflect-metadata': 'reflect-metadata',
         },
       },
     },
