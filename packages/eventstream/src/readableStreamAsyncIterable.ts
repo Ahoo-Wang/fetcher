4e8683ed14e4d40b355434914e30e95fcaa1a63d
Ahoo Wang <ahoowang@qq.com>
2025-09-12 21:16:01 +0800

    feat(eventstream): implement ReadableStreamAsyncIterable
    - Add ReadableStreamAsyncIterable class to support async iteration over ReadableStream
    - Implement AsyncIterable interface with next(), return(), and throw() methods
    - Handle stream reading, error handling, and stream cancellation

---
+++ packages/eventstream/src/readableStreamAsyncIterable.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export class ReadableStreamAsyncIterable<T> implements AsyncIterable<T> {
+  private readonly reader: ReadableStreamDefaultReader<T>;
+
+  constructor(private readonly stream: ReadableStream<T>) {
+    this.reader = stream.getReader();
+  }
+
+  [Symbol.asyncIterator]() {
+    return this;
+  }
+
+  async next(): Promise<IteratorResult<T>> {
+    try {
+      const { done, value } = await this.reader.read();
+      if (done) {
+        this.reader.releaseLock();
+        return { done: true, value: undefined };
+      }
+
+      return { done: false, value };
+    } catch (error) {
+      this.reader.releaseLock();
+      throw error;
+    }
+  }
+
+  async return(): Promise<IteratorResult<T>> {
+    // Release the reader lock when iteration is manually stopped
+    try {
+      await this.stream.cancel();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      this.reader.releaseLock();
+    }
+    return { done: true, value: undefined };
+  }
+
+  async throw(error: any): Promise<IteratorResult<T>> {
+    this.reader.releaseLock();
+    throw error;
+  }
+}
