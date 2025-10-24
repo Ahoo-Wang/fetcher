27a83b58a227794703f38ad98a0f1fcdb3a21888
Ahoo Wang <ahoowang@qq.com>
2025-10-24 16:08:08 +0800

    feat(viewer): add FilterPanel component with storybook examples
    - Created FilterPanel component that renders multiple filters in a grid layout
    - Added storybook stories demonstrating various filter configurations
    - Implemented demo component showing filter condition output
    - Added support for custom actions and column span props
    - Included examples with text, number, and multi-value filters
    - Added documentation describing component functionality

---
+++ packages/viewer/src/filter/panel/stories/FilterPanel.stories.tsx
@@ -0,0 +1,185 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { FilterPanel, FilterPanelProps } from '../FilterPanel';
+import { Condition, Operator } from '@ahoo-wang/fetcher-wow';
+import { useState } from 'react';
+import { Card, Divider, Typography } from 'antd';
+
+function FilterPanelDemo(props: FilterPanelProps) {
+  const [condition, setCondition] = useState<Condition>();
+  return (
+    <Card>
+      <FilterPanel {...props} onSearch={setCondition} />
+      <Divider></Divider>
+      {condition && <Typography.Text code copyable>
+        {JSON.stringify(condition)}
+      </Typography.Text>}
+    </Card>
+  );
+}
+
+const meta: Meta<typeof FilterPanelDemo> = {
+  title: 'Viewer/Filters/Panel/FilterPanel',
+  component: FilterPanelDemo,
+  parameters: {
+    layout: 'padded',
+    docs: {
+      description: {
+        component:
+          'A panel component that renders multiple filters in a grid layout with a search button. It combines individual filters into a single search condition.',
+      },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    filters: [
+      {
+        key: 'name',
+        type: 'text',
+        field: {
+          name: 'name',
+          label: 'Name',
+          type: 'string',
+        },
+        operator: {
+          defaultValue: Operator.EQ,
+        },
+        value: {
+          defaultValue: 'test',
+        },
+      },
+      {
+        key: 'age',
+        type: 'number',
+        field: {
+          name: 'age',
+          label: 'Age',
+          type: 'number',
+        },
+        operator: {
+          defaultValue: Operator.GT,
+        },
+        value: {
+          defaultValue: 18,
+        },
+      },
+    ],
+  },
+};
+
+export const WithActions: Story = {
+  args: {
+    filters: [
+      {
+        key: 'status',
+        type: 'text',
+        field: {
+          name: 'status',
+          label: 'Status',
+          type: 'string',
+        },
+        operator: {
+          defaultValue: Operator.IN,
+        },
+        value: {
+          defaultValue: ['active', 'inactive'],
+        },
+      },
+    ],
+    actions: <button>Reset Filters</button>,
+  },
+};
+
+export const CustomColSpan: Story = {
+  args: {
+    filters: [
+      {
+        key: 'email',
+        type: 'text',
+        field: {
+          name: 'email',
+          label: 'Email',
+          type: 'string',
+        },
+        operator: {
+          defaultValue: Operator.CONTAINS,
+        },
+        value: {
+          defaultValue: '@example.com',
+        },
+      },
+    ],
+    colSpan: 8,
+  },
+};
+
+export const MultipleFilters: Story = {
+  args: {
+    filters: [
+      {
+        key: 'firstName',
+        type: 'text',
+        field: {
+          name: 'firstName',
+          label: 'First Name',
+          type: 'string',
+        },
+        operator: {
+          defaultValue: Operator.STARTS_WITH,
+        },
+        value: {
+          defaultValue: 'John',
+        },
+      },
+      {
+        key: 'lastName',
+        type: 'text',
+        field: {
+          name: 'lastName',
+          label: 'Last Name',
+          type: 'string',
+        },
+        operator: {
+          defaultValue: Operator.ENDS_WITH,
+        },
+        value: {
+          defaultValue: 'Doe',
+        },
+      },
+      {
+        key: 'department',
+        type: 'text',
+        field: {
+          name: 'department',
+          label: 'Department',
+          type: 'string',
+        },
+        operator: {
+          defaultValue: Operator.EQ,
+        },
+        value: {
+          defaultValue: 'Engineering',
+        },
+      },
+    ],
+  },
+};
